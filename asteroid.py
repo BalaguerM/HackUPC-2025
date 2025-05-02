698a16b2fed584420aa794c594e7dfeb967c8f51
BalaguerM <alex.balamar@gmail.com>
2025-05-02 22:39:25 +0200

    code: test code

---
+++ asteroid.py
@@ -0,0 +1,27 @@
+import random
+import math
+
+class Asteroid:
+    def __init__(self, x, y, t):
+        self.x = x
+        self.y = y
+        if t == "Large":
+            self.size = 30
+        elif t == "Normal":
+            self.size = 20
+        else:
+            self.size = 10
+        self.t = t
+
+        # Make random speed and direction
+        self.speed = random.uniform(1, (40 - self.size) * 4 / 15)
+        self.dir = random.randrange(0, 360) * math.pi / 180
+
+        # Make random asteroid sprites
+        full_circle = random.uniform(18, 36)
+        dist = random.uniform(self.size / 2, self.size)
+        self.vertices = []
+        while full_circle < 360:
+            self.vertices.append([dist, full_circle])
+            dist = random.uniform(self.size / 2, self.size)
+            full_circle += random.uniform(18, 36)
